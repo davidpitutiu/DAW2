5a615adca94e37d9c1d255352c8fb098ef443c46
David Pitutiu <56295878+davidpitutiu@users.noreply.github.com>
2021-06-22 02:04:01 +0300

    Add files via upload

---
+++ login/config.php
@@ -0,0 +1,12 @@
+<?php
+    $servername="localhost";
+    $user="root";
+    $pass="";
+    $databasename="socialapp";
+    $connect=mysqli_connect($servername,$user,$pass,$databasename); 
+    /*if(!$connect){
+        echo "Fail";
+    }else{
+        echo "Succes";
+    }*/
+?>
